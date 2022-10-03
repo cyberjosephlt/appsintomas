532c824c79658150a469f1560e59e3b8a66523dc
José Luis Lizalda Torres <lizaldatorres@gmail.com>
2022-10-02 22:59:39 -0500

    //Se carga proyecto de acuerdo a los requerimientos del Sprint3

--- app/src/main/java/co/edu/unab/appsintomas/activity_alumno.java
+++ app/src/main/java/co/edu/unab/appsintomas/activity_alumno.java
@@ -6,10 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.Arrays;
 import java.util.List;
+
+import co.edu.unab.appsintomas.entities.usuario;
 
 public class activity_alumno extends AppCompatActivity {
     private ListView listasintomasa;
@@ -46,6 +51,23 @@ public class activity_alumno extends AppCompatActivity {
         // Set the adapter for ListView
         listasintomasa.setAdapter(adapter);
         listasintomasa.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+
+        //imagen picassoI
+        usuario user= new usuario("56789","Steve Jobs"
+                ,"applebest","manzanita2022","CE",46574,
+                "https://img1.freepng.es/20171220/vwq/steve-jobs-png-5a3a404a148806.97453834151376698608417047.jpg",
+                "inhabilitado","ALUMNO");
+        String urlimg= user.getUrl_foto();
+        ImageView imgdocente= findViewById(R.id.imagenalumno);
+        Picasso.get()
+                .load(urlimg)
+                .placeholder(R.drawable.loading)
+                .error(R.drawable.nofoto)
+                .into(imgdocente);
+
+
+
     }
 
     public void llamar5(View view){
--- app/src/main/java/co/edu/unab/appsintomas/activity_docente.java
+++ app/src/main/java/co/edu/unab/appsintomas/activity_docente.java
@@ -6,10 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.Arrays;
 import java.util.List;
+
+import co.edu.unab.appsintomas.entities.usuario;
 
 public class activity_docente extends AppCompatActivity {
     private ListView listasintomas;
@@ -46,6 +51,21 @@ public class activity_docente extends AppCompatActivity {
         // Set the adapter for ListView
         listasintomas.setAdapter(adapter);
         listasintomas.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+        //imagen picassoI
+        usuario user= new usuario("1234","Bruce Banner"
+                ,"HUlk","enojado2022","CE",56474,
+                "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Mark_Ruffalo_as_%22Professor_Hulk%22.jpeg/1280px-Mark_Ruffalo_as_%22Professor_Hulk%22.jpeg",
+                "habilitado","DOCENTE");
+        String urlimg= user.getUrl_foto();
+        ImageView imgdocente= findViewById(R.id.imagendocente);
+        Picasso.get()
+                .load(urlimg)
+                .placeholder(R.drawable.loading)
+                .error(R.drawable.nofoto)
+                .into(imgdocente);
+
+
     }
 
     public void llamarhd(View view){
